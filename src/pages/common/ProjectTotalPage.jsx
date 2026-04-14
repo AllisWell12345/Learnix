f255b85dbc11ed9b6a44442f4edcae99ba0a9916
01218279393 <478lemontree@gmail.com>
2026-04-14 18:19:37 +0900

    merge를 위한 push

--- src/pages/common/ProjectTotalPage.jsx
+++ src/pages/common/ProjectTotalPage.jsx
@@ -1,8 +1,14 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import "../common/ProjectTotalPage.css";
 
 function ProjectTotalPage() {
-  return <div>ProjectTotalPage</div>;
+  return (
+    <div className="pt-container">
+      <div className="title">
+        <span>프로젝트 조회</span>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectTotalPage;
