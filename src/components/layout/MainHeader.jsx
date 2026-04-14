1b0dbd52ba247e9b3a1f54fb39a0eb9ba51d71e4
always62442 <always22@g.eulji.ac.kr>
2026-04-14 15:30:37 +0900

    header와 footer 코드 작성

--- src/components/layout/MainHeader.jsx
+++ src/components/layout/MainHeader.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./Header.css";
+import logo from "../../assets/img/Learnix-logo.png";
+import loginIcon from "../../assets/img/login-icon.png"; // 로그인 아이콘 이미지 경로
 
 function MainHeader() {
-  return <div>MainHeader</div>;
+  return (
+    <>
+      <header className="header">
+        <div className="inner">
+          <div className="header-left" />
+
+          <div className="header-logo">
+            <img src={logo} alt="Learnix 로고" className="header-logo-img" />
+          </div>
+
+          <div className="header-right">
+            <button className="header-nav-btn">
+              <img src={loginIcon} alt="로그인" className="header-login-img" />
+              <span className="header-nav-text">로그인</span>
+            </button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default MainHeader;
