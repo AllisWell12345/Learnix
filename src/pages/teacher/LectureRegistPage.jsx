60bee06fd8fbecd79488e703effe97fd0f74351d
always62442 <always22@g.eulji.ac.kr>
2026-04-17 13:33:41 +0900

    feature/lecRegistPage

--- src/pages/teacher/LectureRegistPage.jsx
+++ src/pages/teacher/LectureRegistPage.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
 import LectureForm from "../../components/lecture/LectureForm";
 
 function LectureRegistPage() {
   const navigate = useNavigate();
+  // const { user } = useSelector((state) => state.auth);
 
   const handleSubmit = (data) => {
     console.log("등록 데이터:", data);
@@ -10,12 +12,24 @@ function LectureRegistPage() {
     navigate(-1);
   };
 
+  // const handleSubmit = async (data) => {
+  //   try {
+  //     await registLecture(data, user.uid);
+  //     alert("강의가 등록되었습니다!");
+  //     navigate(-1);
+  //   } catch (err) {
+  //     console.error("강의 등록 실패:", err);
+  //     alert("강의 등록 중 오류가 발생했습니다.");
+  //   }
+  // };
+
   const handleCancel = () => {
     navigate(-1);
   };
 
   return (
-    <div className="content"> 강의 등록
+    <div className="content">
+      강의 등록
       <LectureForm onSubmit={handleSubmit} onCancel={handleCancel} />
     </div>
   );
